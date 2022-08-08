b5aa31bac0a061fd3ac1837d1857ed7f8e6705d0
Meldeth <meldeth934@gmail.com>
2022-08-08 13:46:15 +0900

    transition

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const main = document.querySelector('.navbar');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
+    main.classList.toggle('active');
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
